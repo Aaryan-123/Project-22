aaffc242b1d5348107ca6b466ccaae654478eed7
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-16 11:39:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
 	helicopterSprite = createSprite(width / 2, 200, 10, 10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale = 0.6
-
+   
 	groundSprite = createSprite(width / 2, height - 35, width, 10);
 	groundSprite.shapeColor = color(255)
 
